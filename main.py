ec2900f910c6aa303da6e43075e4094c55736863
Froggy <129422220+froggysop@users.noreply.github.com>
2024-09-05 13:23:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ proxies = {
 
 MIN_DELAY = 10  # Minimum delay in seconds
 MAX_DELAY = 30  # Maximum delay in seconds
+MAX_RETRIES = 3
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
